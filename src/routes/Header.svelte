2dd9c3a9a3ed18457c9dac01a9cdbc16ecebf487
sundaram123krishnan <krishnsundaram@gmail.com>
2023-03-28 21:38:50 +0530

    added header

---
+++ src/routes/Header.svelte
@@ -0,0 +1,24 @@
+<div class="mt-10 flex items-center justify-center flex-col gap-5">
+	<div class="flex items-center gap-10">
+		<img src="/apv_logo.png" alt="" class="h-40" />
+		<div class="flex flex-col gap-1.5">
+			<h1 class="text-5xl font-bold  uppercase">Agnel polytechnic , Vashi</h1>
+            <h3 class="text-lg">(Approved by AICTE / Govt. of Maharashtra / DTE & affiliated to MSBTE)</h3>
+            <h3><span class="font-bold">Accredited by NBA</span> (CE & ME)</h3>
+            <h3>DTE Code: <span class="font-bold">3258 </span>| District: <span class="font-bold">Thane</span></h3>
+		</div>
+	</div>
+
+	<ul class="menu menu-horizontal bg-base-100 rounded-box">
+		<li class="uppercase font-bold"><a>Home</a></li>
+		<li class="uppercase font-bold"><a>About Us</a></li>
+		<li class="uppercase font-bold"><a>Courses</a></li>
+		<li class="uppercase font-bold"><a>Achievements</a></li>
+		<li class="uppercase font-bold"><a>Student activities</a></li>
+		<li class="uppercase font-bold"><a>Admission</a></li>
+		<li class="uppercase font-bold"><a>campus</a></li>
+		<li class="uppercase font-bold"><a>downloads</a></li>
+		<li class="uppercase font-bold"><a>alumni</a></li>
+		<li class="uppercase font-bold"><a>circulars</a></li>
+	</ul>
+</div>
