2663c37ede2665ece7fe77e7eeb7cb915fd21900
sundaram123krishnan <krishnsundaram@gmail.com>
2023-03-28 22:17:43 +0530

    partially made header responsive with some bugs

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -13,6 +13,36 @@
 			</h3>
 		</div>
 	</div>
+
+	<div class="drawer xl:hidden">
+		<input id="my-drawer" type="checkbox" class="drawer-toggle" />
+		<div class="drawer-content mx-auto">
+			<!-- Page content here -->
+			<label class="btn" for="my-drawer">
+				<!-- this hidden checkbox controls the state -->
+				<input type="checkbox" id="my-drawer" class="hidden"/><span class="font-bold text-xl">&#9776;</span>
+
+				<!-- hamburger icon -->
+			</label>
+		</div>
+		<div class="drawer-side">
+			<label for="my-drawer" class="drawer-overlay" />
+			<ul class="menu p-4 w-80 bg-base-100 text-base-content">
+				<!-- Sidebar content here -->
+				<li class="uppercase font-semibold"><a href="/">Home</a></li>
+				<li class="uppercase font-semibold"><a href="/">About us</a></li>
+				<li class="uppercase font-semibold"><a href="/">Courses</a></li>
+				<li class="uppercase font-semibold"><a href="/">Achievements</a></li>
+				<li class="uppercase font-semibold"><a href="/">Student activities</a></li>
+				<li class="uppercase font-semibold"><a href="/">Admission</a></li>
+				<li class="uppercase font-semibold"><a href="/">Campus</a></li>
+				<li class="uppercase font-semibold"><a href="/">Downloads</a></li>
+				<li class="uppercase font-semibold"><a href="/">Alumni</a></li>
+				<li class="uppercase font-semibold"><a href="/">Circulars</a></li>
+			</ul>
+		</div>
+	</div>
+
 	<ul class="menu menu-horizontal bg-base-100 rounded-box">
 		<li class="uppercase font-semibold"><a href="/">Home</a></li>
 		<li class="uppercase font-semibold"><a href="/">About us</a></li>
