b5cc7749d707a6581b4dfcda3efa3293bcbfa80a
WhyAsh5114 <whyash5114@gmail.com>
2023-03-28 21:45:35 +0530

    added image slideshow

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script>
 	import Header from './Header.svelte';
+	import Slideshow from './Slideshow.svelte';
 </script>
 
 <Header />
+<Slideshow />
---
+++ src/routes/Slideshow.svelte
@@ -0,0 +1,24 @@
+<div class="carousel w-full mx-auto">
+	<div id="item1" class="carousel-item w-full">
+		<img src="/independece-day.png" class="w-full" alt="banner" />
+	</div>
+	<div id="item2" class="carousel-item w-full">
+		<img src="/uba_banner-7.jpg" class="w-full" alt="banner" />
+	</div>
+	<div id="item3" class="carousel-item w-full">
+		<img src="/uba_banner-10.jpg" class="w-full" alt="banner" />
+	</div>
+	<div id="item4" class="carousel-item w-full">
+		<img src="/uba_banner-11.jpg" class="w-full" alt="banner" />
+	</div>
+	<div id="item5" class="carousel-item w-full">
+		<img src="/uba_banner-12.jpg" class="w-full" alt="banner" />
+	</div>
+</div>
+<div class="flex justify-center w-full py-2 gap-2">
+	<a href="#item1" class="btn btn-sm">1</a>
+	<a href="#item2" class="btn btn-sm">2</a>
+	<a href="#item3" class="btn btn-sm">3</a>
+	<a href="#item4" class="btn btn-sm">4</a>
+	<a href="#item5" class="btn btn-sm">5</a>
+</div>
