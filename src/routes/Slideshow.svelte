f8d00e09eb9e1f720b89219d328b81d1707bcfc4
WhyAsh5114 <whyash5114@gmail.com>
2023-03-29 21:26:49 +0530

    enabled autoplay and added bg to slideshow

--- src/routes/Slideshow.svelte
+++ src/routes/Slideshow.svelte
@@ -6,8 +6,9 @@
 <swiper-container
 	slides-per-view={1}
 	navigation={true}
+	autoplay={true}
 	loop={true}
-	pagination={true}
+	class="bg-stone-800 border-2 border-gray-500"
 >
 	<swiper-slide><img src="uba_banner-7.jpg" alt="banner" class="mx-auto" /></swiper-slide>
 	<swiper-slide><img src="uba_banner-10.jpg" alt="banner" class="mx-auto" /></swiper-slide>
