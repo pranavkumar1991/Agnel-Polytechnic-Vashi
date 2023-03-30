74c9d14cedc7d19abaf4717d10642e08ff8e1ac5
WhyAsh5114 <whyash5114@gmail.com>
2023-03-30 17:57:41 +0530

    using card component from daisyUI

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,19 +1,23 @@
-<div class="flex items-center justify-center flex-col mt-5 gap-2 mb-3">
-	<h2 class="uppercase text-4xl font-bold">about agnels</h2>
-	<p class="text-lg  ml-32 mr-32">
-		The founder of Agnel ashram, Fr. C. Rodrigues was a great visionary. His vision was to foster
-		love and understanding among the various communities in India and to contribute to the
-		development of self-reliance among the youth through education.
-		<br />
-        <br />
-		Agnel Polytechnic in Vashi, Navi Mumbai, was started in 1983 with only one program – Diploma in civil
-		engineering. It has grown since then, and today we have four branches of study in the polytechnic
-		– Diploma in Civil Engineering, Dip. in Mechanical Engineering, Dip. in Automobile engineering and
-		Dip. in Electronics and Telecommunication.
-		<br />
-        <br />
-		What makes Agnel Polytechnic a unique institution in Mumbai is its discipline and culture and the
-		dedication of the faculty in imparting knowledge and expertise to the students in a cosmopolitan
-		atmosphere.
-	</p>
+<div class="card lg:card-side bg-base-100 shadow-xl max-w-screen-2xl mx-auto my-12 w-full border">
+	<figure class="w-full"><img src="/agnel_campus.webp" alt="campus" /></figure>
+	<div class="card-body">
+		<h2 class="card-title text-3xl font-bold">About Agnels</h2>
+		<div class="h-px bg-black" />
+		<p class="text-lg">
+			The founder of Agnel ashram, Fr. C. Rodrigues was a great visionary. His vision was to foster
+			love and understanding among the various communities in India and to contribute to the
+			development of self-reliance among the youth through education.
+			<br />
+			<br />
+			Agnel Polytechnic in Vashi, Navi Mumbai, was started in 1983 with only one program - Diploma in
+			civil engineering. It has grown since then, and today we have four branches of study in the polytechnic
+			- Diploma in Civil Engineering, Dip. in Mechanical Engineering, Dip. in Automobile engineering
+			and Dip. in Electronics and Telecommunication.
+			<br />
+			<br />
+			What makes Agnel Polytechnic a unique institution in Mumbai is its discipline and culture and the
+			dedication of the faculty in imparting knowledge and expertise to the students in a cosmopolitan
+			atmosphere.
+		</p>
+	</div>
 </div>
