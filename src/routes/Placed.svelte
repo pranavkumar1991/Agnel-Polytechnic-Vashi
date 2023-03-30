f49938bd0360fddb22bbe3a99d1fff1c4fa0d249
sundaram123krishnan <krishnsundaram@gmail.com>
2023-03-30 17:31:01 +0530

    added placement section

---
+++ src/routes/Placed.svelte
@@ -0,0 +1,10 @@
+<div class="flex justify-center items-center flex-col gap-5 mt-5">
+	<h2 class="uppercase font-bold text-3xl">Our Students have been placed in:</h2>
+	<div class="flex justify-center items-center">
+		<img src="/toyo_eng.png" alt="" />
+		<img src="/godrej.png" alt="" />
+		<img src="/larsen&turbo.png" alt="" />
+		<img src="/hyundai.png" alt="" />
+        
+	</div>
+</div>
