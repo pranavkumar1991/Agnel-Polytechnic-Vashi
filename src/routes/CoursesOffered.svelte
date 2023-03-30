290e2bd44942846aee5abe76f818f7b7517846f5
WhyAsh5114 <whyash5114@gmail.com>
2023-03-30 17:57:53 +0530

    added title

--- src/routes/CoursesOffered.svelte
+++ src/routes/CoursesOffered.svelte
@@ -1,44 +1,47 @@
-<div class="overflow-x-auto">
-	<table class="table table-compact w-full max-w-7xl mx-auto">
-		<thead>
-			<tr>
-				<th>No.</th>
-				<th>Programs offered</th>
-				<th>Code</th>
-				<th>Intake</th>
-			</tr>
-		</thead>
-		<tbody>
-            <tr>
-				<th>1</th>
-				<td>Artificial Intelligence & Machine Learning</td>
-				<td>AN</td>
-				<td>60</td>
-			</tr>
-			<tr>
-				<th>2</th>
-				<td>Civil Engineering</td>
-				<td>CE</td>
-				<td>60</td>
-			</tr>
-			<tr>
-				<th>3</th>
-				<td>Automobile Engineering</td>
-				<td>AE</td>
-				<td>60</td>
-			</tr>
-			<tr>
-				<th>4</th>
-				<td>Electronics & Computer Engineering</td>
-				<td>TE</td>
-				<td>60</td>
-			</tr>
-			<tr>
-				<th>5</th>
-				<td>Mechanical Engineering</td>
-				<td>ME</td>
-				<td>120</td>
-			</tr>
-		</tbody>
-	</table>
+<div class="w-full max-w-5xl mx-auto shadow-md shadow-stone-700 p-3">
+	<h3 class="mx-auto text-2xl uppercase font-bold flex w-fit bg-primary text-white py-3 px-16 mb-2 rounded-md">Courses offered</h3>
+	<div class="overflow-x-auto">
+		<table class="table table-compact w-full max-w-7xl mx-auto">
+			<thead>
+				<tr>
+					<th>No.</th>
+					<th>Programs offered</th>
+					<th>Code</th>
+					<th>Intake</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<th>1</th>
+					<td>Artificial Intelligence & Machine Learning</td>
+					<td>AN</td>
+					<td>60</td>
+				</tr>
+				<tr>
+					<th>2</th>
+					<td>Civil Engineering</td>
+					<td>CE</td>
+					<td>60</td>
+				</tr>
+				<tr>
+					<th>3</th>
+					<td>Automobile Engineering</td>
+					<td>AE</td>
+					<td>60</td>
+				</tr>
+				<tr>
+					<th>4</th>
+					<td>Electronics & Computer Engineering</td>
+					<td>TE</td>
+					<td>60</td>
+				</tr>
+				<tr>
+					<th>5</th>
+					<td>Mechanical Engineering</td>
+					<td>ME</td>
+					<td>120</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
 </div>
