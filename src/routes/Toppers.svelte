f1b1543657b012548ed094a19de49b4be1daa703
sundaram123krishnan <krishnsundaram@gmail.com>
2023-03-30 17:31:15 +0530

    added toppers section

---
+++ src/routes/Toppers.svelte
@@ -0,0 +1,4 @@
+<div class="flex items-center justify-center mt-10 bg-blue-600 w-fit mx-auto">
+	<img src="/Our-toppers.jpg" alt="toppers" />
+	<h3 class="uppercase text-3xl px-6 text-white">Join the list</h3>
+</div>
