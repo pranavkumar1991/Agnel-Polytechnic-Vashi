afa16cf5ef3a5f64a5e574f733e7e8691b431081
sundaram123krishnan <krishnsundaram@gmail.com>
2023-03-30 16:12:31 +0530

    Added about section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,11 @@
 	import CoursesOffered from './CoursesOffered.svelte';
 	import Header from './Header.svelte';
 	import Slideshow from './Slideshow.svelte';
+	import About from './about.svelte';
 </script>
 
 <Header>
 	<Slideshow />
 </Header>
+<About />
 <CoursesOffered />
---
+++ src/routes/about.svelte
@@ -0,0 +1,19 @@
+<div class="flex items-center justify-center flex-col mt-5 gap-2 mb-3">
+	<h2 class="uppercase text-4xl font-bold">about agnels</h2>
+	<p class="text-lg  ml-32 mr-32">
+		The founder of Agnel ashram, Fr. C. Rodrigues was a great visionary. His vision was to foster
+		love and understanding among the various communities in India and to contribute to the
+		development of self-reliance among the youth through education.
+		<br />
+        <br />
+		Agnel Polytechnic in Vashi, Navi Mumbai, was started in 1983 with only one program – Diploma in civil
+		engineering. It has grown since then, and today we have four branches of study in the polytechnic
+		– Diploma in Civil Engineering, Dip. in Mechanical Engineering, Dip. in Automobile engineering and
+		Dip. in Electronics and Telecommunication.
+		<br />
+        <br />
+		What makes Agnel Polytechnic a unique institution in Mumbai is its discipline and culture and the
+		dedication of the faculty in imparting knowledge and expertise to the students in a cosmopolitan
+		atmosphere.
+	</p>
+</div>
