5cb717627ce913b0e51beb90ee19e4ec01d26ccf
WhyAsh5114 <whyash5114@gmail.com>
2023-03-31 23:42:15 +0530

    added allCourses section

---
+++ src/routes/AllCourses.svelte
@@ -0,0 +1,38 @@
+<h3 class="text-center text-4xl font-bold my-16 uppercase divider">COURSES</h3>
+<div class="grid gap-5 grid-cols-1 md:grid-cols-2 xl:grid-cols-3 max-w-4xl xl:max-w-7xl mx-auto justify-items-center">
+	<div class="card w-96 bg-base-100 shadow-xl">
+		<figure><img src="/AN_logo.png" alt="artificial intelligence" /></figure>
+		<div class="card-body">
+			<h2 class="card-title">Artificial Intelligence & Machine Learning (AN)</h2>
+			<p>Intake - 60</p>
+		</div>
+	</div>
+    <div class="card w-96 bg-base-100 shadow-xl">
+		<figure><img src="/TE_logo.jpg" alt="computer engineering" /></figure>
+		<div class="card-body">
+			<h2 class="card-title">Electronics & Computer Engineering (TE)</h2>
+			<p>Intake - 60</p>
+		</div>
+	</div>
+    <div class="card w-96 bg-base-100 shadow-xl">
+		<figure><img src="/CE_logo.jpg" alt="civil engineering" /></figure>
+		<div class="card-body">
+			<h2 class="card-title">Civil Engineering (CE)</h2>
+			<p>Intake - 60</p>
+		</div>
+	</div>
+    <div class="card w-96 bg-base-100 shadow-xl">
+		<figure><img src="/AE_logo.jpg" alt="automobile engineering" /></figure>
+		<div class="card-body">
+			<h2 class="card-title">Automobile Engineering (AE)</h2>
+			<p>Intake - 60</p>
+		</div>
+	</div>
+    <div class="card w-96 bg-base-100 shadow-xl">
+		<figure><img src="/ME_logo.jpg" alt="mechanical engineering stock" /></figure>
+		<div class="card-body">
+			<h2 class="card-title">Mechanical Engineering (ME)</h2>
+			<p>Intake - 120</p>
+		</div>
+	</div>
+</div>
