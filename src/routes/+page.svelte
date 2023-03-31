001453644477191ec2968fae8fdcf95cf4fbf3a2
sundaram123krishnan <krishnsundaram@gmail.com>
2023-03-31 21:44:47 +0530

    added hero

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,5 @@
+<script>
+	import Hero from './Hero.svelte';
+</script>
 
+<Hero />
---
+++ src/routes/Hero.svelte
@@ -0,0 +1,77 @@
+<div class="carousel w-full">
+	<div id="slide1" class="carousel-item relative w-full">
+		<div class="hero min-h-screen" style="background-image: url(/library.jpeg);">
+			<div class="hero-overlay bg-opacity-60" />
+			<div class="hero-content text-center text-neutral-content">
+				<div class="max-w-xl">
+					<h1 class="mb-5 text-5xl font-bold">Agnel Polytechnic, Vashi</h1>
+					<p class="mb-5">
+						(Approved by AICTE / Govt. of Maharashtra / DTE & affiliated to MSBTE)
+						<br />
+						Accredited by NBA (CE & ME)
+						<br />
+						DTE Code: 3258 / District - Thane
+					</p>
+				</div>
+			</div>
+		</div>
+		<div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+			<a href="#slide4" class="btn btn-circle">❮</a>
+			<a href="#slide2" class="btn btn-circle">❯</a>
+		</div>
+	</div>
+	<div id="slide2" class="carousel-item relative w-full">
+		<div class="hero min-h-screen" style="background-image: url(/library.jpeg);">
+			<div class="hero-overlay bg-opacity-60" />
+			<div class="hero-content text-center text-neutral-content">
+				<div class="max-w-md">
+					<h1 class="mb-5 text-5xl font-bold">Agnel Polytechnic , Vashi</h1>
+					<p class="mb-5">
+						Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi
+						exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
+					</p>
+				</div>
+			</div>
+		</div>
+		<div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+			<a href="#slide1" class="btn btn-circle">❮</a>
+			<a href="#slide3" class="btn btn-circle">❯</a>
+		</div>
+	</div>
+	<div id="slide3" class="carousel-item relative w-full">
+		<div class="hero min-h-screen" style="background-image: url(/library.jpeg);">
+			<div class="hero-overlay bg-opacity-60" />
+			<div class="hero-content text-center text-neutral-content">
+				<div class="max-w-md">
+					<h1 class="mb-5 text-5xl font-bold">Agnel Polytechnic , Vashi</h1>
+					<p class="mb-5">
+						Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi
+						exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
+					</p>
+				</div>
+			</div>
+		</div>
+		<div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+			<a href="#slide2" class="btn btn-circle">❮</a>
+			<a href="#slide4" class="btn btn-circle">❯</a>
+		</div>
+	</div>
+	<div id="slide4" class="carousel-item relative w-full">
+		<div class="hero min-h-screen" style="background-image: url(/library.jpeg);">
+			<div class="hero-overlay bg-opacity-60" />
+			<div class="hero-content text-center text-neutral-content">
+				<div class="max-w-md">
+					<h1 class="mb-5 text-5xl font-bold">Agnel Polytechnic , Vashi</h1>
+					<p class="mb-5">
+						Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi
+						exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
+					</p>
+				</div>
+			</div>
+		</div>
+		<div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+			<a href="#slide3" class="btn btn-circle">❮</a>
+			<a href="#slide1" class="btn btn-circle">❯</a>
+		</div>
+	</div>
+</div>
