b5926e8960a5955bedf15431b7913fe9cd2395c7
WhyAsh5114 <whyash5114@gmail.com>
2023-03-31 21:13:56 +0530

    revamp

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,1 @@
-<script>
-	import CoursesOffered from './CoursesOffered.svelte';
-	import Header from './Header.svelte';
-	import Slideshow from './Slideshow.svelte';
-	import About from './About.svelte';
-	import Placed from './Placed.svelte'
-	import Toppers from './Toppers.svelte'
-	import Footer from './Footer.svelte'
-</script>
 
-<Header>
-	<Slideshow />
-</Header>
-<About />
-<CoursesOffered />
-<Placed />
-<Toppers />
-<Footer />
--- src/routes/Toppers.svelte
+++
@@ -1,4 +0,0 @@
-<div class="flex items-center mt-10 bg-primary w-full max-w-screen-2xl mx-auto shadow-xl border">
-	<img src="/Our-toppers.jpg" alt="toppers" />
-	<h3 class="text-7xl uppercase font-bold px-6 text-white text-center w-full">Join the list!</h3>
-</div>
