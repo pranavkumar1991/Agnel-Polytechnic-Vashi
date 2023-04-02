fe20c09c48e3114ff8e2aa51d1ab79cdc3968e6d
sundaram123krishnan <krishnsundaram@gmail.com>
2023-04-02 17:44:36 +0530

    fixed responsiveness issue of floating buttons

--- src/routes/FloatingButtons.svelte
+++ src/routes/FloatingButtons.svelte
@@ -1,15 +1,15 @@
 <script>
 	import FaBookOpen from 'svelte-icons/fa/FaBookOpen.svelte';
-	import FaFlask from 'svelte-icons/fa/FaFlask.svelte';
+	import GoBeaker from 'svelte-icons/go/GoBeaker.svelte';
 	import MdPeople from 'svelte-icons/md/MdPeople.svelte';
 </script>
 
 <div
-	class="btn-group btn-group-vertical lg:btn-group-horizontal gap-4 lg:gap-20 lg:mx-96 lg:relative lg:-top-48 w-full"
+	class="btn-group btn-group-vertical btn-group-horizontal grid grid-cols-1 gap-3 md:grid-cols-1 relative lg:grid-cols-3 lg:-top-64 max-w-3xl px-3 my-10 lg:my-0 lg:max-w-6xl mx-auto "
 >
 	<button class="btn h-max hover:bg-primary border-none">
 		<div class="card card-side shadow-xl">
-			<figure class="w-24 p-1"><FaBookOpen /></figure>
+			<figure class="w-24"><FaBookOpen /></figure>
 			<div class="card-body">
 				<h2 class="card-title">All Courses</h2>
 				<div class="card-actions">
@@ -20,7 +20,7 @@
 	</button>
 	<button class="btn h-max hover:bg-primary border-none">
 		<div class="card card-side shadow-xl">
-			<figure class="w-28 p-5"><FaFlask /></figure>
+			<figure class="w-24"><GoBeaker /></figure>
 			<div class="card-body">
 				<h2 class="card-title">Laboratories</h2>
 				<div class="card-actions">
@@ -33,7 +33,7 @@
 		<div class="card card-side shadow-xl">
 			<figure class="w-24"><MdPeople /></figure>
 			<div class="card-body">
-				<h2 class="card-title">Our Faculty</h2>
+				<h2 class="card-title">Our Staff</h2>
 				<div class="card-actions">
 					<button class="btn btn-accent btn-sm">View more</button>
 				</div>
