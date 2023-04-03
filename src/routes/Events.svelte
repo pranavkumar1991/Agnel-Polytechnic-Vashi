d7aa9533b80b7bd64e7dfc2807e029c1a6a4080d
WhyAsh5114 <whyash5114@gmail.com>
2023-04-03 21:29:26 +0530

    prettier

--- src/routes/Events.svelte
+++ src/routes/Events.svelte
@@ -1,43 +1,43 @@
-<h3 class="text-center text-4xl font-bold mb-12 mt-16 divider">Events</h3>
+<h2 class="text-center text-4xl font-bold mb-12 mt-16 divider">Events</h2>
 <div class="grid w-full max-w-7xl mx-auto place-items-center lg:grid-cols-2 gap-5">
-    <div class="card lg:card-side bg-accent text-white max-h-80 shadow-xl max-w-2xl w-full">
-        <figure><img src="/events/resonance.jpeg" alt="Resonance 2023" /></figure>
-        <div class="card-body lg:basis-5/12 shrink-0">
-            <h2 class="card-title text-xl lg:text-3xl">Resonance 2023</h2>
-            <p class="text-md lg:text-lg lg:pt-3">20th Feb - 24th Feb</p>
-            <div class="card-actions justify-end">
-                <a class="btn btn-primary btn-sm lg:btn-md">Gallery</a>
-            </div>
-        </div>
-    </div>
-    <div class="card lg:card-side bg-accent text-white max-h-80 shadow-xl max-w-2xl w-full">
-        <figure><img src="/events/zest.png" alt="Zest 2023" /></figure>
-        <div class="card-body lg:basis-5/12 shrink-0">
-            <h2 class="card-title text-xl lg:text-3xl">Zest 2023</h2>
-            <p class="text-md lg:text-lg lg:pt-3">20th Feb - 24th Feb</p>
-            <div class="card-actions justify-end">
-                <a class="btn btn-primary btn-sm lg:btn-md">Gallery</a>
-            </div>
-        </div>
-    </div>
-    <div class="card lg:card-side bg-accent text-white max-h-80 shadow-xl max-w-2xl w-full">
-        <figure><img src="/events/resonance.jpeg" alt="Resonance 2023" /></figure>
-        <div class="card-body lg:basis-5/12 shrink-0">
-            <h2 class="card-title text-xl lg:text-3xl">Technocratz 2022</h2>
-            <p class="text-md lg:text-lg lg:pt-3">20th Feb - 24th Feb</p>
-            <div class="card-actions justify-end">
-                <a class="btn btn-primary btn-sm lg:btn-md">Gallery</a>
-            </div>
-        </div>
-    </div>
-    <div class="card lg:card-side bg-accent text-white max-h-80 shadow-xl max-w-2xl w-full">
-        <figure><img src="/events/resonance.jpeg" alt="Resonance 2023" /></figure>
-        <div class="card-body lg:basis-5/12 shrink-0">
-            <h2 class="card-title text-xl lg:text-3xl">Technocratz 2022</h2>
-            <p class="text-md lg:text-lg lg:pt-3">20th Feb - 24th Feb</p>
-            <div class="card-actions justify-end">
-                <a class="btn btn-primary btn-sm lg:btn-md">Gallery</a>
-            </div>
-        </div>
-    </div>
+	<div class="card lg:card-side bg-accent text-white max-h-80 shadow-xl max-w-2xl w-full">
+		<figure><img src="/events/resonance.jpeg" alt="Resonance 2023" /></figure>
+		<div class="card-body lg:basis-5/12 shrink-0">
+			<h2 class="card-title text-xl lg:text-3xl">Resonance 2023</h2>
+			<p class="text-md lg:text-lg lg:pt-3">20th Feb - 24th Feb</p>
+			<div class="card-actions justify-end">
+				<a class="btn btn-primary btn-sm lg:btn-md">Gallery</a>
+			</div>
+		</div>
+	</div>
+	<div class="card lg:card-side bg-accent text-white max-h-80 shadow-xl max-w-2xl w-full">
+		<figure><img src="/events/zest.png" alt="Zest 2023" /></figure>
+		<div class="card-body lg:basis-5/12 shrink-0">
+			<h2 class="card-title text-xl lg:text-3xl">Zest 2023</h2>
+			<p class="text-md lg:text-lg lg:pt-3">20th Feb - 24th Feb</p>
+			<div class="card-actions justify-end">
+				<a class="btn btn-primary btn-sm lg:btn-md">Gallery</a>
+			</div>
+		</div>
+	</div>
+	<div class="card lg:card-side bg-accent text-white max-h-80 shadow-xl max-w-2xl w-full">
+		<figure><img src="/events/resonance.jpeg" alt="Resonance 2023" /></figure>
+		<div class="card-body lg:basis-5/12 shrink-0">
+			<h2 class="card-title text-xl lg:text-3xl">Technocratz 2022</h2>
+			<p class="text-md lg:text-lg lg:pt-3">20th Feb - 24th Feb</p>
+			<div class="card-actions justify-end">
+				<a class="btn btn-primary btn-sm lg:btn-md">Gallery</a>
+			</div>
+		</div>
+	</div>
+	<div class="card lg:card-side bg-accent text-white max-h-80 shadow-xl max-w-2xl w-full">
+		<figure><img src="/events/resonance.jpeg" alt="Resonance 2023" /></figure>
+		<div class="card-body lg:basis-5/12 shrink-0">
+			<h2 class="card-title text-xl lg:text-3xl">Technocratz 2022</h2>
+			<p class="text-md lg:text-lg lg:pt-3">20th Feb - 24th Feb</p>
+			<div class="card-actions justify-end">
+				<a class="btn btn-primary btn-sm lg:btn-md">Gallery</a>
+			</div>
+		</div>
+	</div>
 </div>
--- src/routes/FloatingButtons.svelte
+++ src/routes/FloatingButtons.svelte
@@ -1,6 +1,6 @@
 <script>
 	import FaBookOpen from 'svelte-icons/fa/FaBookOpen.svelte';
-	import IoIosFlask from 'svelte-icons/io/IoIosFlask.svelte'
+	import IoIosFlask from 'svelte-icons/io/IoIosFlask.svelte';
 	import MdPeople from 'svelte-icons/md/MdPeople.svelte';
 	import { fly } from 'svelte/transition';
 
@@ -14,7 +14,7 @@
 	<div
 		class="btn-group btn-group-vertical lg:btn-group-horizontal grid grid-cols-1 gap-3 md:grid-cols-1 relative lg:grid-cols-3 lg:-top-64 max-w-3xl px-3 my-10 lg:my-0 lg:max-w-6xl mx-auto"
 	>
-		<button
+		<a
 			class="btn btn-secondary h-max border-none animate-none"
 			in:fly={{ x: -200, y: 400, duration: 1000 }}
 		>
@@ -23,26 +23,23 @@
 				<div class="card-body">
 					<h2 class="card-title">All Courses</h2>
 					<div class="card-actions">
-						<button class="btn rounded-md btn-accent btn-sm">View more</button>
+						<a class="btn rounded-md btn-accent btn-sm">View more</a>
 					</div>
 				</div>
 			</div>
-		</button>
-		<button
-			class="btn btn-secondary h-max border-none animate-none"
-			in:fly={{ y: 400, duration: 1500 }}
-		>
+		</a>
+		<a class="btn btn-secondary h-max border-none animate-none" in:fly={{ y: 400, duration: 1500 }}>
 			<div class="card card-side text-black">
 				<figure class="basis-1/3 shrink-0 px-1"><IoIosFlask /></figure>
 				<div class="card-body">
 					<h2 class="card-title">Laboratories</h2>
 					<div class="card-actions">
-						<button class="btn rounded-md btn-accent btn-sm">View more</button>
+						<a class="btn rounded-md btn-accent btn-sm">View more</a>
 					</div>
 				</div>
 			</div>
-		</button>
-		<button
+		</a>
+		<a
 			class="btn btn-secondary h-max border-none animate-none"
 			in:fly={{ x: 200, y: 400, duration: 1750 }}
 		>
@@ -51,10 +48,10 @@
 				<div class="card-body">
 					<h2 class="card-title">Our Staff</h2>
 					<div class="card-actions">
-						<button class="btn rounded-md btn-accent btn-sm">View more</button>
+						<a class="btn rounded-md btn-accent btn-sm">View more</a>
 					</div>
 				</div>
 			</div>
-		</button>
+		</a>
 	</div>
 {/if}
