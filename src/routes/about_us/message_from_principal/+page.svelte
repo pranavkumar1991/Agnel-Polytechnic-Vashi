f607fe2d8608e32d0f57a29f8f5c5ccc0b66788b
sundaram123krishnan <krishnsundaram@gmail.com>
2023-04-06 22:31:19 +0530

    added about for principal and managing director

---
+++ src/routes/about_us/message_from_principal/+page.svelte
@@ -0,0 +1,41 @@
+<div class="flex flex-col w-full border-opacity-50 mt-40">
+	<div class="lg:divider text-5xl font-bold px-2">Message from Principal</div>
+</div>
+<div class="hero max-w-7xl mx-auto ring-secondary ring-8 rounded-xl shadow-2xl m-10">
+	<div class="hero-content flex-col lg:flex-row gap-10">
+		<div class="card card-compact basis-1/3 shrink-0 bg-base-100 shadow-xl">
+			<figure>
+				<img src="/principal.png" alt="Principal madam" />
+			</figure>
+			<div class="card-body">
+				<h2 class="card-title">Ms. Saly Antony</h2>
+			</div>
+		</div>
+		<div>
+			<h1 class="text-5xl font-bold">Welcome to Agnel Family</h1>
+			<p class="py-6 text-lg">
+				Agnel Polytechnic was established in 1983 by Agnel Ashram Fathers as the first unit of Agnel
+				Technical Education Complex. The Complex has grown over the years and has shown marked
+				progress in various fields due to the dedicated service and leadership of Rev. Fr. Orlando
+				and Rev. Fr. Almeida. Our institute was the first Polytechnic to be accredited in Navi
+				Mumbai.
+				<br />
+				<br />
+				Education is that which forms character, strengthens mind, expands the intellect and helps one
+				to stand on one’s feet. A cohesive team of well-qualified, dedicated and sincere staff are committed
+				to focused vision of imparting quality education and wholesome growth of students. Most of our
+				students pass out with flying colours every year and a good percentage of them enroll for higher
+				studies. There are many successful entrepreneurs in various fields from our Alumni. We have a
+				Center for Incubation and Business Acceleration(CIBA) which gives support and guidance to our
+				young entrepreneurs to transfer their ideas into business.
+				<br />
+				<br />
+				The institute has good infrastructural facilities. All laboratories are well equipped with modern
+				instruments/machinery. In order to have effective industry-institute interaction, we have signed
+				MOUs with industries, the major one being the tie-up signed with Hyundai Motors India Ltd. (HMIL).
+				HMIL has developed an Automobile Lab and Training Center to make sure that the skills imparted
+				by the institute keep in pace with the industry and produces quality manpower.
+			</p>
+		</div>
+	</div>
+</div>
