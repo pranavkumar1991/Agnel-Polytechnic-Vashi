fdd58c6b3b0f79fc22b0da34e06557e1dc233f46
WhyAsh5114 <whyash5114@gmail.com>
2023-04-08 00:01:37 +0530

    split up into different components better modularization

--- src/routes/about_us/vision_and_messages/+page.svelte
+++ src/routes/about_us/vision_and_messages/+page.svelte
@@ -1,72 +1,11 @@
+<script>
+	import MessageFromManagingDirector from './MessageFromManagingDirector.svelte';
+	import MessageFromPrincipal from './MessageFromPrincipal.svelte';
+	import VisionAndMission from './VisionAndMission.svelte';
+</script>
+
 <div class="grid gap-20">
-	<section class="grid grid-cols-2 gap-10 mx-auto max-w-7xl">
-		<div class="flex flex-col col-span-2 w-full border-opacity-50">
-			<div class="lg:divider text-5xl font-bold px-2">Vision and mission</div>
-		</div>
-		<div class="card shadow-xl mx-auto max-w-2xl bg-secondary text-black">
-			<div class="card-body">
-				<h2 class="card-title mx-auto text-4xl">Vision</h2>
-				<p class="text-lg text-justify">
-					To evolve and flourish as a progressive center for modern technical education, stirring in
-					every student’s creativity, leading to entrepreneurship through holistic development,
-					nurtured by the strength and legitimate pride of Indian values and the spirit of true
-					service.
-				</p>
-			</div>
-		</div>
-		<div class="card shadow-xl mx-auto max-w-2xl bg-secondary text-black">
-			<div class="card-body">
-				<h2 class="card-title mx-auto text-4xl">Mission</h2>
-				<ul class="text-lg list-disc mx-auto">
-					<li>Providing industry oriented quality education and training.</li>
-					<li>Providing a holistic environment for overall personal development.</li>
-					<li>Bringing to full blossoming the potential in every student.</li>
-				</ul>
-			</div>
-		</div>
-	</section>
-	<section>
-		<div class="flex flex-col w-full border-opacity-50">
-			<div class="lg:divider text-5xl font-bold px-2">Message from Principal</div>
-		</div>
-		<div class="hero max-w-7xl mx-auto bg-accent rounded-xl shadow-2xl m-10 text-white">
-			<div class="hero-content flex-col lg:flex-row gap-10">
-				<div class="card card-compact basis-1/3 shrink-0 bg-base-100 shadow-xl">
-					<figure>
-						<img src="/principal.png" alt="Principal madam" />
-					</figure>
-					<div class="card-body">
-						<h2 class="card-title text-black">Ms. Saly Antony</h2>
-					</div>
-				</div>
-				<div>
-					<h1 class="text-3xl font-bold">Welcome to Agnel Family</h1>
-					<p class="py-6">
-						Agnel Polytechnic was established in 1983 by Agnel Ashram Fathers as the first unit of
-						Agnel Technical Education Complex. The Complex has grown over the years and has shown
-						marked progress in various fields due to the dedicated service and leadership of Rev.
-						Fr. Orlando and Rev. Fr. Almeida. Our institute was the first Polytechnic to be
-						accredited in Navi Mumbai.
-						<br />
-						<br />
-						Education is that which forms character, strengthens mind, expands the intellect and helps
-						one to stand on one’s feet. A cohesive team of well-qualified, dedicated and sincere staff
-						are committed to focused vision of imparting quality education and wholesome growth of students.
-						Most of our students pass out with flying colours every year and a good percentage of them
-						enroll for higher studies. There are many successful entrepreneurs in various fields from
-						our Alumni. We have a Center for Incubation and Business Acceleration(CIBA) which gives support
-						and guidance to our young entrepreneurs to transfer their ideas into business.
-						<br />
-						<br />
-						The institute has good infrastructural facilities. All laboratories are well equipped with
-						modern instruments/machinery. In order to have effective industry-institute interaction,
-						we have signed MOUs with industries, the major one being the tie-up signed with Hyundai Motors
-						India Ltd. (HMIL). HMIL has developed an Automobile Lab and Training Center to make sure
-						that the skills imparted by the institute keep in pace with the industry and produces quality
-						manpower.
-					</p>
-				</div>
-			</div>
-		</div>
-	</section>
+	<VisionAndMission />
+	<MessageFromPrincipal />
+	<MessageFromManagingDirector />
 </div>
---
+++ src/routes/about_us/vision_and_messages/VisionAndMission.svelte
@@ -0,0 +1,24 @@
+<section class="grid grid-cols-2 gap-10 mx-auto max-w-7xl">
+	<h2 class="text-5xl font-bold col-span-2 divider">Vision and Mission</h2>
+	<div class="card shadow-xl bg-secondary text-black">
+		<div class="card-body">
+			<h2 class="card-title mx-auto text-4xl font-bold">Vision</h2>
+			<p class="text-lg text-justify">
+				To evolve and flourish as a progressive center for modern technical education, stirring in
+				every student's creativity, leading to entrepreneurship through holistic development,
+				nurtured by the strength and legitimate pride of Indian values and the spirit of true
+				service.
+			</p>
+		</div>
+	</div>
+	<div class="card shadow-xl bg-secondary text-black">
+		<div class="card-body">
+			<h2 class="card-title mx-auto text-4xl font-bold">Mission</h2>
+			<ul class="text-lg list-disc mx-auto pl-2">
+				<li>Providing industry oriented quality education and training.</li>
+				<li>Providing a holistic environment for overall personal development.</li>
+				<li>Bringing to full blossoming the potential in every student.</li>
+			</ul>
+		</div>
+	</div>
+</section>
