be6d20f529a772a84cad97a97c6e3391d0909e53
WhyAsh5114 <whyash5114@gmail.com>
2023-04-08 00:01:20 +0530

    prettier

--- src/routes/PlacementStats.svelte
+++ src/routes/PlacementStats.svelte
@@ -3,11 +3,31 @@
 >
 	<h1 class="font-bold text-5xl">Our students have been placed in</h1>
 	<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
-		<img src="/placement_companies/blue_star.png" alt="blue star" class="rounded-lg border-2 border-accent" />
+		<img
+			src="/placement_companies/blue_star.png"
+			alt="blue star"
+			class="rounded-lg border-2 border-accent"
+		/>
 		<img src="/placement_companies/toyo.png" alt="toyo" class="rounded-lg border-2 border-accent" />
-		<img src="/placement_companies/l&t.png" alt="larsen and toubro" class="rounded-lg border-2 border-accent" />
-		<img src="/placement_companies/voltas.png" alt="voltas" class="rounded-lg border-2 border-accent" />
-		<img src="/placement_companies/godrej.png" alt="godrej" class="rounded-lg border-2 border-accent" />
-		<img src="/placement_companies/hyundai.png" alt="hyundai" class="rounded-lg border-2 border-accent" />
+		<img
+			src="/placement_companies/l&t.png"
+			alt="larsen and toubro"
+			class="rounded-lg border-2 border-accent"
+		/>
+		<img
+			src="/placement_companies/voltas.png"
+			alt="voltas"
+			class="rounded-lg border-2 border-accent"
+		/>
+		<img
+			src="/placement_companies/godrej.png"
+			alt="godrej"
+			class="rounded-lg border-2 border-accent"
+		/>
+		<img
+			src="/placement_companies/hyundai.png"
+			alt="hyundai"
+			class="rounded-lg border-2 border-accent"
+		/>
 	</div>
 </div>
