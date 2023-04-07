15ef76a3b5e56b243130edb981371a486a4504e6
WhyAsh5114 <whyash5114@gmail.com>
2023-04-08 00:00:43 +0530

    cleanup

--- src/routes/AboutAgnel.svelte
+++ src/routes/AboutAgnel.svelte
@@ -7,19 +7,16 @@
 				The founder of Agnel ashram, Fr. C. Rodrigues was a great visionary. His vision was to
 				foster love and understanding among the various communities in India and to contribute to
 				the development of self-reliance among the youth through education.
-				<br />
-				<br />
-				Agnel Polytechnic in Vashi, Navi Mumbai, was started in 1983 with only one program – Diploma
+				<br /><br />
+				Agnel Polytechnic in Vashi, Navi Mumbai, was started in 1983 with only one program - Diploma
 				in civil engineering. It has grown since then, and today we have four branches of study in the
-				polytechnic – Diploma in Civil Engineering, Dip. in Mechanical Engineering, Dip. in Automobile
+				polytechnic - Diploma in Civil Engineering, Dip. in Mechanical Engineering, Dip. in Automobile
 				engineering and Dip. in Electronics and Telecommunication.
-				<br />
-				<br />
+				<br /><br />
 				What makes Agnel Polytechnic a unique institution in Mumbai is its discipline and culture and
 				the dedication of the faculty in imparting knowledge and expertise to the students in a cosmopolitan
 				atmosphere.
 			</p>
-			<a class="btn btn-accent">Read More</a>
 		</div>
 	</div>
 </div>
