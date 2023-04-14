16f73c4da1a1f543e8f7069667f2a07666756041
sundaram123krishnan <krishnsundaram@gmail.com>
2023-04-14 23:48:12 +0530

    added students mentoring section with image

---
+++ src/routes/about_us/students_mentoring/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import Staff from './Staff.svelte';
+	let staff = [
+		['Ms. Roopa Shetty', '	Hum', 'Overall Coordinator', '7039021128'],
+		['Mr. Lewis Anthony', 'Civil', 'Member', '9004897775'],
+		['Ms. Suhasini David', 'EXTC', 'Member', '9819321626'],
+		['Mr. Nitin Kulkarni', 'ME', 'Member', '9969338603'],
+		['Mr. Satish Eandole', 'AE', '	Member', '9834833416'],
+		['Ms. Roopali Hedge', '', 'Counsellor', '9619325880']
+	];
+</script>
+
+<div class="overflow-x-auto w-full flex flex-col gap-8 items-center">
+	<h2 class="text-5xl font-bold lg:col-span-2 lg:divider">Students Mentoring and Grievance Cell</h2>
+	<p class="max-w-7xl text-center">
+		Students Mentoring and Grievance Cell have been formed in order to solve the problems of the students and to motivate them to do well in Academic
+		as well as extra/ Co-curricular activities.
+		<br />
+		<br />
+		The following are the members of this cell and students are requested to contact them in case of any difficulty/ Suggestions.
+	</p>
+	<table class="table w-full">
+		<thead>
+			<tr>
+				<th>Name of the staff</th>
+				<th>Course</th>
+				<th>Contact Number</th>
+			</tr>
+		</thead>
+
+		<Staff data={staff} />
+	</table>
+</div>
