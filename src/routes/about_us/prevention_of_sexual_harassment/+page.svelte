c32bf48e375bd61aa3e443651205cf325541bbc8
sundaram123krishnan <krishnsundaram@gmail.com>
2023-04-14 23:47:38 +0530

    added prevention against sexual harassment section

---
+++ src/routes/about_us/prevention_of_sexual_harassment/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import Staff from './Staff.svelte';
+	let staff = [
+		['Mrs. Beenu Johnkutty', 'Chairman', '	9869441160'],
+		['Mrs. Nirmala M.', '	Member', '	9819879923'],
+		['	Mr. Upendra Rai', '	Member', '	9869358567'],
+		['	Mrs. Swapnali Thottam', '	Member', '	9969895227'],
+		['	Mr. Avinash Jage', '	Member', '	704596720'],
+		['	Dr. Dipti Joshi', 'Member from NGO Sreemukti Sanghatana', '	8369765171'],
+		['Ms. Swarali Kharche', '	Student (EJ-5I)', '	9623134810'],
+		['Mr. Varun P.', '	Student (ME-3I)', '9326858981'],
+		['Ms. Anjali Survade', 'Student (CE-5I)', '	8108158885']
+	];
+</script>
+
+<div class="overflow-x-auto w-full flex flex-col gap-8 items-center">
+	<h2 class="text-5xl font-bold lg:col-span-2 lg:divider">ICC – Prevention of sexual harassment</h2>
+	<table class="table w-full">
+		<thead>
+			<tr>
+				<th>Name of the staff</th>
+				<th>Designation</th>
+				<th>Contact Number</th>
+			</tr>
+		</thead>
+
+		<Staff data={staff} />
+	</table>
+</div>
