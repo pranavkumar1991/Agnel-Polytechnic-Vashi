358a8aefbf7ac8ff4f87453d0b05bd556c327f96
WhyAsh5114 <whyash5114@gmail.com>
2023-04-17 21:54:51 +0530

    added links to mobile about us page

--- src/routes/about_us/+page.svelte
+++ src/routes/about_us/+page.svelte
@@ -1,5 +1,9 @@
 <ul class="menu w-56 rounded-box bg-secondary text-black mx-auto font-semibold border-2 border-black">
 	<li class="border-b-2 border-black"><a href="/about_us/vision_and_messages">Vision and messages</a></li>
 	<li class="border-b-2 border-black"><a href="/about_us/staff_details">Staff details</a></li>
-	<li><a href="/about_us/anti_ragging">Anti ragging</a></li>
+	<li class="border-b-2 border-black"><a href="/about_us/anti_ragging">Anti ragging</a></li>
+	<li class="border-b-2 border-black"><a href="/about_us/ladies_grievance">Ladies grievance</a></li>
+	<li class="border-b-2 border-black"><a href="/about_us/prevention_of_sexual_harassment">Prevention of sexual harassment</a></li>
+	<li class="border-b-2 border-black"><a href="/about_us/students_mentoring">Students mentoring</a></li>
+	<li><a href="/about_us/institution_innovation_council">Institution innovation council</a></li>
 </ul>
