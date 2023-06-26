9ef043dc32a00e4569f7610b3db129f28e497f6c
WhyAsh5114 <whyash5114@gmail.com>
2023-06-26 23:08:27 +0530

    UI changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,13 @@
 <script>
 	import Hero from './Hero.svelte';
-	import FloatingButtons from './FloatingButtons.svelte';
 	import AllCourses from './AllCourses.svelte';
 	import AboutAgnel from './AboutAgnel.svelte';
-	import Stats from './Stats.svelte';
 	import Events from './Events.svelte';
 	import PlacementStats from './PlacementStats.svelte';
 </script>
 
 <div class="-mt-36">
 	<Hero />
-	<FloatingButtons />
 	<AllCourses />
 	<AboutAgnel />
 	<Events />
