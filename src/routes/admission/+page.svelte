58f9ca490476c9539935a71a2c6fad79c942dd4d
sundaram123krishnan <krishnsundaram@gmail.com>
2023-06-26 22:40:47 +0530

    added admission section

---
+++ src/routes/admission/+page.svelte
@@ -0,0 +1,52 @@
+<div class="flex flex-col align-center justify-center p-10 gap-20">
+    <div class="flex flex-col gap-5">
+        <h1 class="divider-heading">Admission Procedure</h1>
+        <p class="text-lg">For Admission to CAP Seats as well as Institutional Quota, it is compulsory for all candidates to purchase an application kit and submit the online application form. Please verify that your name appears in the merit list prepared by the Competent Authority nominated by the Govt. of Maharashtra.</p>
+    </div>
+    <div class="flex flex-col gap-5 text-lg">
+        <h1 class="divider-heading">Centralized Admission Process (CAP)</h1>
+        <p>Centralised Admission Process- (1) The Unaided Private Professional Education Institution shall admit Candidates through the Centralised Admission Process as per the rules. The stages of CAP shall be as stated below,-</p>
+        <ul class="list-disc">
+            <li>Display or publishing of Information Brochure by the Competent Authority.</li>
+            <li>Filling Online Application Form by Candidate for participation in the Centralised Admission Process.</li>
+            <li>Confirmation of Online Application Form and Document Verification at Facilitation Centre by the Candidate in person.</li>
+            <li>Display or publishing of Provisional merit lists, Submission of grievances, if any, and Display or publishing of Final Merit Lists.</li>
+            <li>Display of available Categorywise Seats (Seat Matrix) for CAP Rounds I, II, and III.</li>
+            <li>Filling up and Confirmation of Online Option Form having preferences of Courses and Institutions before CAP Round I.</li>
+        </ul>
+        <p>Candidates may fill in choices of Institutes and Courses in decreasing order of their preference as specified by Competent Authority. The option form once confirmed shall be considered for allotment in CAP Round I, II, and III.</p>
+        <ul class="list-disc">
+            <li>Display of Provisional Allotment of CAP Round I, II, and III indicating allotted institute and Course.</li>
+            <li>Reporting and accepting the offered seat will be as per the allotment of the CAP Round I, II, and III by the Candidate at Reporting Centre.</li>
+        </ul>
+    </div>
+    <div class="flex flex-col gap-5 text-lg">
+        <h1 class="divider-heading">During the CAP</h1>
+        <p>Before accepting the allotted seat a Candidate can opt to ‘Freeze’, ‘Slide’ or ‘Float’ the choice of an academic program for the subsequent round(s), if any, of seat allocation, as provided below:-</p>
+        <ul class="list-disc">
+            <li>Freeze Candidates accept the offered seat and they do not want to participate in any further rounds of seat allocation. Such Candidates will not be considered in subsequent rounds of admission</li>
+            <li>Slide Candidates accept the offered seat and indicate that, if admission to the academic program of higher preference is offered within the same Institution, they will accept it. Such Candidates will be considered in subsequent rounds of admission</li>
+            <li>Float Candidates accept the offered seat and indicate that, if admission to an academic program of higher preference in any Institution is offered, they will accept it. Else, they will continue with the currently accepted academic program. Such Candidates will be considered in subsequent rounds of admission</li>
+            <li>For Candidates who choose the Float or Slide option, and secures a seat of his choice of higher preference, then it would automatically result in the forfeiture of the seat accepted by the Candidate in the earlier round</li>
+            <li>Sliding and Floating options will not be available to a candidate who is offered admission to his first choice.</li>
+        </ul>
+    </div>
+    <div class="flex flex-col gap-5 text-lg">
+        <h1 class="divider-heading">Admission in Institutional Quota</h1>
+        <p>The Institution can admit eligible candidates against 20% of sanctioned intake. Aspiring candidates can apply to the Principal for admission at the Institute Level.</p>
+    </div>
+    <div class="flex flex-col gap-5 text-lg">
+        <h1 class="divider-heading">Institute Level Round</h1>
+        <p>If the seats remain vacant after all CAP rounds, the said seats will be filled in by the Institution through the Institute Level round on the basis of the Intense merit of the candidates applying for the same.</p>
+    </div>
+    <div class="flex flex-col gap-5 text-lg">
+        <h1 class="divider-heading">Refund of Fees</h1>
+        <p>Refund of fees after cancellation of admission by Institutions.</p>
+        <ul class="list-disc">
+            <li>The Candidate shall apply online for cancellation and submit a signed copy of system generated application for cancellation of admission to the institution.</li>
+            <li>After receiving an admission cancellation request from the Candidate, the institution shall cancel the admission immediately and generate online acknowledgment of cancellation of admission through institution login and issue a signed copy to the Candidate.</li>
+            <li>The refund of fees before the cut-off date of admission shall be made within two days i.e. total fee minus the processing charges of Rs. 1,000/-, or refund rule shall be as per the guidelines of the appropriate authorities or the State Government as per the case may be.</li>
+            <li>If the admission is canceled after the cut-off date of admission declared by the Competent Authority, there shall be no refund except the Security Deposit and Caution Money Deposit.</li>
+        </ul>
+    </div>
+</div>
