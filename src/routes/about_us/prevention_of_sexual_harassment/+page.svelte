41d59edebef76dac356265ad5dd4b55dc67784de
WhyAsh5114 <whyash5114@gmail.com>
2023-06-26 23:10:27 +0530

    data clean

--- src/routes/about_us/prevention_of_sexual_harassment/+page.svelte
+++ src/routes/about_us/prevention_of_sexual_harassment/+page.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
 	import Staff from './Staff.svelte';
 	let staff = [
-		['Mrs. Beenu Johnkutty', 'Chairman', '	9869441160'],
-		['Mrs. Nirmala M.', '	Member', '	9819879923'],
-		['	Mr. Upendra Rai', '	Member', '	9869358567'],
-		['	Mrs. Swapnali Thottam', '	Member', '	9969895227'],
-		['	Mr. Avinash Jage', '	Member', '	704596720'],
-		['	Dr. Dipti Joshi', 'Member from NGO Sreemukti Sanghatana', '	8369765171'],
-		['Ms. Swarali Kharche', '	Student (EJ-5I)', '	9623134810'],
+		['Mrs. Beenu Johnkutty', 'Chairman', '9869441160'],
+		['Mrs. Nirmala M.', 'Member', '9819879923'],
+		['Mr. Upendra Rai', 'Member', '9869358567'],
+		['Mrs. Swapnali Thottam', 'Member', '9969895227'],
+		['Mr. Avinash Jage', 'Member', '704596720'],
+		['Dr. Dipti Joshi', 'Member from NGO Sreemukti Sanghatana', '8369765171'],
+		['Ms. Swarali Kharche', 'Student (EJ-5I)', '9623134810'],
 		['Mr. Varun P.', '	Student (ME-3I)', '9326858981'],
 		['Ms. Anjali Survade', 'Student (CE-5I)', '	8108158885']
 	];
