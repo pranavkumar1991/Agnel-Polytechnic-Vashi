2f5db478a9111526644b4f32da7c820d91031c96
WhyAsh5114 <whyash5114@gmail.com>
2023-06-27 22:44:17 +0530

    fixed image width

--- src/routes/about_us/institution_innovation_council/+page.svelte
+++ src/routes/about_us/institution_innovation_council/+page.svelte
@@ -9,19 +9,21 @@
 	];
 </script>
 
-<div class="flex flex-col gap-4 mb-8">
+<div class="flex flex-col gap-4 mb-8 px-2">
 	<h2 class="divider-heading lg:col-span-2">Institution’s Innovation Council (IIC)</h2>
-	<img src="/icc_image.jpg" alt="IIC logo" />
-	<p class="max-w-7xl">
-		Established Institution’s Innovation Council under ‘MHRD’s Innovation Cell (MIC) to systematically foster the culture of Innovation amongst
-		students.
-		<br />
-		<br />
-		The IIC promotes innovation in the Institution through multitudinous modes leading to an innovation promotion eco-system in the campus. Last year for
-		the National Innovation Contest a total of 947 PoCs were submitted by IICs all over India including IITs, NITs, Engg Colleges, etc. We are glad to
-		announce that the PoC submitted by our students “Early detection of cancer with non-invasive technique” from the Electronics Department under the guidance
-		of Mr. Umesh Mhapankar got selected for the National Boot Camp and ours was the only Polytechnic in the selected list.
-	</p>
+	<div class="flex flex-col lg:flex-row items-center gap-10 mb-8">
+		<img src="/icc_image.jpg" alt="IIC logo" width="800" />
+		<p class="max-w-7xl">
+			Established Institution’s Innovation Council under ‘MHRD’s Innovation Cell (MIC) to systematically foster the culture of Innovation amongst
+			students.
+			<br />
+			<br />
+			The IIC promotes innovation in the Institution through multitudinous modes leading to an innovation promotion eco-system in the campus. Last year for
+			the National Innovation Contest a total of 947 PoCs were submitted by IICs all over India including IITs, NITs, Engg Colleges, etc. We are glad to
+			announce that the PoC submitted by our students “Early detection of cancer with non-invasive technique” from the Electronics Department under the guidance
+			of Mr. Umesh Mhapankar got selected for the National Boot Camp and ours was the only Polytechnic in the selected list.
+		</p>
+	</div>
 	<h2 class="lg:col-span-2 divider-heading">IIC committee</h2>
 </div>
 <div class="overflow-x-auto w-full">
