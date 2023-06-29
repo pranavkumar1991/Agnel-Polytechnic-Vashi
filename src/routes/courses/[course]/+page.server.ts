10237a53eb6a0cbfa42ccc24f3e5c2320de6021e
WhyAsh5114 <whyash5114@gmail.com>
2023-06-29 21:27:26 +0530

    modified page

--- src/routes/courses/[course]/+page.server.ts
+++ src/routes/courses/[course]/+page.server.ts
@@ -1,6 +1,7 @@
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 import { data } from '../data';
+import fs from 'fs';
 
 export const load = (async ({ params }) => {
 	const deptData = data[params.course];
@@ -9,7 +10,14 @@ export const load = (async ({ params }) => {
 		throw error(404, 'Not found');
 	}
 
+	const directories = fs.readdirSync(`./static/courses/${params.course}`);
+	const items: Record<string, string[]> = {};
+	directories.forEach((directory) => {
+		items[directory] = fs.readdirSync(`./static/courses/${params.course}/${directory}`);
+	});
+
 	return {
-		deptData
+		deptData,
+		items
 	};
 }) satisfies PageServerLoad;
