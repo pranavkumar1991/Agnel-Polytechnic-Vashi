3606439d347135944d552e8d037ea635d5c1dc08
WhyAsh5114 <whyash5114@gmail.com>
2023-07-10 22:15:15 +0530

    fixed gallery

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 	export let data;
 
-	let selectedDirectory = 'Facilities';
+	let selectedDirectory = $page.params['section'] ? $page.params['section'] : 'Facilities';
 </script>
 
 <div class="join flex justify-center border border-black p-2 bg-accent mx-auto w-full max-w-7xl overflow-x-auto">
@@ -17,8 +18,17 @@
 	{/each}
 </div>
 
-<div class="grid items-center grid-cols-2 gap-5 m-10">
+<ul class="flex flex-wrap mt-10 gap-0.5">
 	{#each data.items[selectedDirectory] as imgName}
-		<img src="/gallery/{selectedDirectory}/{imgName}" alt={imgName} />
+		<li style="height: 40vh;" class="grow">
+			<img src="/gallery/{selectedDirectory}/{imgName}" alt={imgName} class="max-h-full min-w-full object-cover align-bottom" />
+		</li>
 	{/each}
-</div>
+	<li />
+</ul>
+
+<style>
+	li:last-child {
+		flex-grow: 10;
+	}
+</style>
