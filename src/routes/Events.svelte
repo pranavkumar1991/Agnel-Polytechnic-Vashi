e6ad96ee579d53c8df56ccc13381175df1047483
WhyAsh5114 <whyash5114@gmail.com>
2023-07-10 22:51:54 +0530

    UI changes removed extra event details

--- src/routes/Events.svelte
+++ src/routes/Events.svelte
@@ -1,12 +1,12 @@
 <h2 class="text-center text-4xl font-bold mb-12 mt-16 divider max-w-7xl mx-auto">Events</h2>
 <div class="grid w-full max-w-7xl mx-auto place-items-center lg:grid-cols-2 gap-5">
 	<div class="card lg:card-side bg-accent text-white max-h-80 shadow-xl max-w-2xl w-full">
-		<figure><img src="/events/resonance.jpeg" alt="Resonance 2023" /></figure>
+		<figure><img src="/events/resonance.jpg" alt="Resonance 2023" /></figure>
 		<div class="card-body lg:basis-5/12 shrink-0">
 			<h2 class="card-title text-xl lg:text-3xl">Resonance 2023</h2>
 			<p class="text-md lg:text-lg lg:pt-3">20th Feb - 24th Feb</p>
 			<div class="card-actions justify-end">
-				<a class="btn btn-primary btn-sm lg:btn-md">Gallery</a>
+				<a class="btn btn-primary btn-sm" href="/gallery?section=ResoZest2023">Gallery</a>
 			</div>
 		</div>
 	</div>
@@ -16,27 +16,7 @@
 			<h2 class="card-title text-xl lg:text-3xl">Zest 2023</h2>
 			<p class="text-md lg:text-lg lg:pt-3">20th Feb - 24th Feb</p>
 			<div class="card-actions justify-end">
-				<a class="btn btn-primary btn-sm lg:btn-md">Gallery</a>
-			</div>
-		</div>
-	</div>
-	<div class="card lg:card-side bg-accent text-white max-h-80 shadow-xl max-w-2xl w-full">
-		<figure><img src="/events/resonance.jpeg" alt="Resonance 2023" /></figure>
-		<div class="card-body lg:basis-5/12 shrink-0">
-			<h2 class="card-title text-xl lg:text-3xl">Technocratz 2022</h2>
-			<p class="text-md lg:text-lg lg:pt-3">20th Feb - 24th Feb</p>
-			<div class="card-actions justify-end">
-				<a class="btn btn-primary btn-sm lg:btn-md">Gallery</a>
-			</div>
-		</div>
-	</div>
-	<div class="card lg:card-side bg-accent text-white max-h-80 shadow-xl max-w-2xl w-full">
-		<figure><img src="/events/resonance.jpeg" alt="Resonance 2023" /></figure>
-		<div class="card-body lg:basis-5/12 shrink-0">
-			<h2 class="card-title text-xl lg:text-3xl">Technocratz 2022</h2>
-			<p class="text-md lg:text-lg lg:pt-3">20th Feb - 24th Feb</p>
-			<div class="card-actions justify-end">
-				<a class="btn btn-primary btn-sm lg:btn-md">Gallery</a>
+				<a class="btn btn-primary btn-sm" href="/gallery?section=ResoZest2023">Gallery</a>
 			</div>
 		</div>
 	</div>
