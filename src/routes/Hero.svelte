a8f5f26895b07e8d00bbe8ec6e6233ca3908ffe0
WhyAsh5114 <whyash5114@gmail.com>
2023-07-10 21:55:33 +0530

    added overlay

--- src/routes/Hero.svelte
+++ src/routes/Hero.svelte
@@ -1,54 +1,42 @@
-<div class="carousel w-full text-white text-opacity-90">
-	<div id="slide1" class="carousel-item relative w-full">
-		<div class="hero min-h-screen" style="background-image: url(/heroImages/Hero1.JPG);">
-			<div class="hero-overlay bg-opacity-50" />
-			<div class="hero-content text-center">
-				<div class="max-w-3xl">
-					<img src="/apv_logo.png" alt="logo" class="w-32 md:w-48 mx-auto" />
-					<h1 class="mb-5 text-4xl md:text-5xl font-bold white">Agnel Polytechnic, Vashi</h1>
-					<p class="mb-5 text-sm md:text-base">
-						( Approved by AICTE | Govt. of Maharashtra | DTE & affiliated to MSBTE )
-						<br />
-						Accredited by NBA (CE & ME)
-						<br />
-						DTE Code: 3258 | District: Thane
-					</p>
-				</div>
-			</div>
-		</div>
-		<div class="absolute flex justify-between transform -translate-y-1/2 left-1 right-1 top-2/3 md:top-1/2">
-			<a href="#slide1" class="btn btn-circle">❮</a>
-			<a href="#slide2" class="btn btn-circle">❯</a>
-		</div>
+<div class="relative h-screen">
+	<div class="flex flex-col items-center text-white h-screen justify-center mx-auto left-16 right-16 z-10 absolute">
+		<img src="/apv_logo.png" alt="logo" class="w-32 md:w-48 mx-auto" />
+		<h1 class="mb-5 text-4xl md:text-5xl font-bold white">Agnel Polytechnic, Vashi</h1>
+		<p class="mb-5 text-sm md:text-base text-center">
+			( Approved by AICTE | Govt. of Maharashtra | DTE & affiliated to MSBTE )
+			<br />
+			Accredited by NBA (CE & ME)
+			<br />
+			DTE Code: 3258 | District: Thane
+		</p>
 	</div>
-	<div id="slide2" class="carousel-item relative w-full">
-		<div class="hero min-h-screen" style="background-image: url(/heroImages/Hero2.JPG);">
-			<div class="hero-overlay bg-opacity-50" />
-			<div class="hero-content text-center">
-				<div class="max-w-3xl">
-					<h1 class="mb-5 text-4xl md:text-5xl font-bold whitespace-nowrap">Agnel Polytechnic,</h1>
-					<h1 class="mb-5 text-4xl md:text-5xl font-bold whitespace-nowrap">Vashi</h1>
-				</div>
+	<div class="absolute carousel w-full text-white text-opacity-90">
+		<div id="slide1" class="carousel-item relative w-full">
+			<div class="hero min-h-screen" style="background-image: url(/heroImages/Hero1.JPG);">
+				<div class="hero-overlay bg-opacity-50" />
+			</div>
+			<div class="absolute flex justify-between transform -translate-y-1/2 left-1 right-1 top-2/3 md:top-1/2">
+				<a href="#slide1" class="btn btn-circle">❮</a>
+				<a href="#slide2" class="btn btn-circle">❯</a>
 			</div>
 		</div>
-		<div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-2/3 md:top-1/2">
-			<a href="#slide1" class="btn btn-circle">❮</a>
-			<a href="#slide3" class="btn btn-circle">❯</a>
-		</div>
-	</div>
-	<div id="slide3" class="carousel-item relative w-full">
-		<div class="hero min-h-screen" style="background-image: url(/heroImages/Hero3.JPG);">
-			<div class="hero-overlay bg-opacity-50" />
-			<div class="hero-content text-center">
-				<div class="max-w-3xl">
-					<h1 class="mb-5 text-4xl md:text-5xl font-bold whitespace-nowrap">Agnel Polytechnic,</h1>
-					<h1 class="mb-5 text-4xl md:text-5xl font-bold whitespace-nowrap">Vashi</h1>
-				</div>
+		<div id="slide2" class="carousel-item relative w-full">
+			<div class="hero min-h-screen" style="background-image: url(/heroImages/Hero2.JPG);">
+				<div class="hero-overlay bg-opacity-50" />
+			</div>
+			<div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-2/3 md:top-1/2">
+				<a href="#slide1" class="btn btn-circle">❮</a>
+				<a href="#slide3" class="btn btn-circle">❯</a>
 			</div>
 		</div>
-		<div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-2/3 md:top-1/2">
-			<a href="#slide2" class="btn btn-circle">❮</a>
-			<a href="#slide1" class="btn btn-circle">❯</a>
+		<div id="slide3" class="carousel-item relative w-full">
+			<div class="hero min-h-screen" style="background-image: url(/heroImages/Hero3.JPG);">
+				<div class="hero-overlay bg-opacity-50" />
+			</div>
+			<div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-2/3 md:top-1/2">
+				<a href="#slide2" class="btn btn-circle">❮</a>
+				<a href="#slide1" class="btn btn-circle">❯</a>
+			</div>
 		</div>
 	</div>
 </div>
