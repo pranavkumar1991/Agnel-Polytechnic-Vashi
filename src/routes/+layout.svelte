64f6c3cdd7978ca2f8266a1d8d34d7f59b8a723e
WhyAsh5114 <whyash5114@gmail.com>
2023-07-11 19:26:44 +0530

    removed animation and hacky fix for scroll history

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,146 +2,106 @@
 	import '../app.postcss';
 	import FaPhoneAlt from 'svelte-icons/fa/FaPhoneAlt.svelte';
 	import IoMdMenu from 'svelte-icons/io/IoMdMenu.svelte';
-	import { fade } from 'svelte/transition';
 	import Footer from './Footer.svelte';
-	import { beforeNavigate, afterNavigate } from '$app/navigation';
-
-	let animateNow = false;
-	setTimeout(() => {
-		animateNow = true;
-	}, 1);
-
-	let scrollHistory = [];
-
-	beforeNavigate((navigation) => {
-		// I have everything wrapped in a `main` tag so that's the part I need to scroll
-		const main = document.querySelector('main');
-
-		scrollHistory.push({
-			to: navigation.to,
-			from: navigation.from,
-			scrollY: main.scrollTop
-		});
-	});
-
-	afterNavigate((navigation) => {
-		const main = document.querySelector('main');
-		const routeHistory = scrollHistory.find((history) => {
-			return history.from.url.pathname === navigation.to.url.pathname;
-		});
-
-		// I only want to revert the scroll when going back in the history
-		if (routeHistory && navigation.type == 'popstate') {
-			main.scrollTo(0, routeHistory.scrollY);
-			// I reset the scrollHistory here so that the array doesn't store lots of unneeded values, but could also just add and remove the necessary ones if the page structure is more complex
-			scrollHistory = [];
-		} else {
-			// if it's a page that isn't in the scrollHistory, simply scroll to the top of the page
-			main.scrollTo(0, 0);
-		}
-	});
 </script>
 
-{#if animateNow}
-	<div
-		class="navbar bg-primary md:w-auto md:left-20 md:right-20 2xl:left-40 2xl:right-40 top-3 md:top-7 lg:top-10 absolute z-50 rounded-lg border-2 border-black shadow-lg"
-		in:fade
-	>
-		<div class="navbar-start">
-			<div class="dropdown">
-				<button class="btn btn-ghost btn-sm 2xl:hidden" aria-label="Menu">
-					<IoMdMenu />
-				</button>
-				<ul class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 font-semibold border border-black">
-					<li><a href="/">HOME</a></li>
-					<li>
-						<details class="dropdown">
-							<summary class="">ABOUT US</summary>
-							<ul class="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-auto border border-black">
-								<li>
-									<a href="/about_us/vision_and_messages">Vision and messages</a>
-									<a href="/about_us/staff_details">Staff details</a>
-									<a href="/about_us/anti_ragging">Anti ragging</a>
-									<a href="/about_us/ladies_grievance">Ladies grievance</a>
-									<a href="/about_us/prevention_of_sexual_harassment">Prevention of sexual harassment</a>
-									<a href="/about_us/students_mentoring">Students mentoring</a>
-									<a href="/about_us/institution_innovation_council">Institution innovation council</a>
-								</li>
-							</ul>
-						</details>
-					</li>
-					<li>
-						<details class="dropdown">
-							<summary class="">COURSES</summary>
-							<ul class="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-auto border border-black">
-								<li>
-									<a href="/courses/artificial_intelligence">Artificial Intelligence</a>
-									<a href="/courses/electronics_and_computer_engineering">Electronics & Computer Engineering</a>
-									<a href="/courses/civil_engineering">Civil Engineering</a>
-									<a href="/courses/automobile_engineering">Automobile Engineering</a>
-									<a href="/courses/mechanical_engineering">Mechanical Engineering</a>
-								</li>
-							</ul>
-						</details>
-					</li>
-					<li><a href="/gallery">GALLERY</a></li>
-					<li><a href="/admissions">ADMISSIONS</a></li>
-					<li><a href="/syllabus">SYLLABUS</a></li>
-					<li><a href="/contact_us">CONTACT US</a></li>
-				</ul>
-			</div>
-			<a class="btn btn-ghost normal-case text-xl gap-2" href="/">
-				<img src="/apv_logo.png" alt="Logo" class="w-12" />
-				<h1 class="text-left 22xl:hidden">APV</h1>
-				<h1 class="text-left hidden 22xl:block">Agnel Polytechnic</h1>
-			</a>
-		</div>
-		<div class="navbar-center hidden 2xl:flex">
-			<ul class="menu menu-horizontal px-1 font-semibold">
+<nav
+	class="navbar bg-primary md:w-auto md:left-20 md:right-20 2xl:left-40 2xl:right-40 top-3 md:top-7 lg:top-10 absolute z-50 rounded-lg border-2 border-black shadow-lg"
+>
+	<div class="navbar-start">
+		<div class="dropdown">
+			<button class="btn btn-ghost btn-sm 2xl:hidden" aria-label="Menu">
+				<IoMdMenu />
+			</button>
+			<ul class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 font-semibold border border-black">
 				<li><a href="/">HOME</a></li>
-				<li class="dropdown dropdown-hover">
-					<button tabindex="0">ABOUT US</button>
-					<ul class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-auto border-accent border-2">
-						<li>
-							<a href="/about_us/vision_and_messages">Vision and messages</a>
-							<a href="/about_us/staff_details">Staff details</a>
-							<a href="/about_us/anti_ragging">Anti ragging</a>
-							<a href="/about_us/ladies_grievance">Ladies grievance</a>
-							<a href="/about_us/prevention_of_sexual_harassment">Prevention of sexual harassment</a>
-							<a href="/about_us/students_mentoring">Students mentoring</a>
-							<a href="/about_us/institution_innovation_council">Institution innovation council</a>
-						</li>
-					</ul>
+				<li>
+					<details class="dropdown">
+						<summary class="">ABOUT US</summary>
+						<ul class="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-auto border border-black">
+							<li>
+								<a href="/about_us/vision_and_messages">Vision and messages</a>
+								<a href="/about_us/staff_details">Staff details</a>
+								<a href="/about_us/anti_ragging">Anti ragging</a>
+								<a href="/about_us/ladies_grievance">Ladies grievance</a>
+								<a href="/about_us/prevention_of_sexual_harassment">Prevention of sexual harassment</a>
+								<a href="/about_us/students_mentoring">Students mentoring</a>
+								<a href="/about_us/institution_innovation_council">Institution innovation council</a>
+							</li>
+						</ul>
+					</details>
 				</li>
-				<li class="dropdown dropdown-hover">
-					<button tabindex="0">COURSES</button>
-					<ul class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-auto border-accent border-2">
-						<li>
-							<a href="/courses/artificial_intelligence">Artificial Intelligence</a>
-							<a href="/courses/electronics_and_computer_engineering">Electronics & Computer Engineering</a>
-							<a href="/courses/civil_engineering">Civil Engineering</a>
-							<a href="/courses/automobile_engineering">Automobile Engineering</a>
-							<a href="/courses/mechanical_engineering">Mechanical Engineering</a>
-						</li>
-					</ul>
+				<li>
+					<details class="dropdown">
+						<summary class="">COURSES</summary>
+						<ul class="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-auto border border-black">
+							<li>
+								<a href="/courses/artificial_intelligence">Artificial Intelligence</a>
+								<a href="/courses/electronics_and_computer_engineering">Electronics & Computer Engineering</a>
+								<a href="/courses/civil_engineering">Civil Engineering</a>
+								<a href="/courses/automobile_engineering">Automobile Engineering</a>
+								<a href="/courses/mechanical_engineering">Mechanical Engineering</a>
+							</li>
+						</ul>
+					</details>
 				</li>
-				<li><a href="/gallery">GALLERY</a></li>
+				<li><a href="/gallery/Facilities">GALLERY</a></li>
 				<li><a href="/admissions">ADMISSIONS</a></li>
 				<li><a href="/syllabus">SYLLABUS</a></li>
 				<li><a href="/contact_us">CONTACT US</a></li>
 			</ul>
 		</div>
-		<div class="navbar-end">
-			<a class="btn gap-3 rounded-sm btn-accent" href="tel:02241611000">
-				<div class="w-5">
-					<FaPhoneAlt />
-				</div>
-				022 4161 1000
-			</a>
-		</div>
+		<a class="btn btn-ghost normal-case text-xl gap-2" href="/">
+			<img src="/apv_logo.png" alt="Logo" class="w-12" />
+			<h1 class="text-left 22xl:hidden">APV</h1>
+			<h1 class="text-left hidden 22xl:block">Agnel Polytechnic</h1>
+		</a>
 	</div>
-{/if}
-<div class="mt-36">
+	<div class="navbar-center hidden 2xl:flex">
+		<ul class="menu menu-horizontal px-1 font-semibold">
+			<li><a href="/">HOME</a></li>
+			<li class="dropdown dropdown-hover">
+				<button tabindex="0">ABOUT US</button>
+				<ul class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-auto border-accent border-2">
+					<li>
+						<a href="/about_us/vision_and_messages">Vision and messages</a>
+						<a href="/about_us/staff_details">Staff details</a>
+						<a href="/about_us/anti_ragging">Anti ragging</a>
+						<a href="/about_us/ladies_grievance">Ladies grievance</a>
+						<a href="/about_us/prevention_of_sexual_harassment">Prevention of sexual harassment</a>
+						<a href="/about_us/students_mentoring">Students mentoring</a>
+						<a href="/about_us/institution_innovation_council">Institution innovation council</a>
+					</li>
+				</ul>
+			</li>
+			<li class="dropdown dropdown-hover">
+				<button tabindex="0">COURSES</button>
+				<ul class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-auto border-accent border-2">
+					<li>
+						<a href="/courses/artificial_intelligence">Artificial Intelligence</a>
+						<a href="/courses/electronics_and_computer_engineering">Electronics & Computer Engineering</a>
+						<a href="/courses/civil_engineering">Civil Engineering</a>
+						<a href="/courses/automobile_engineering">Automobile Engineering</a>
+						<a href="/courses/mechanical_engineering">Mechanical Engineering</a>
+					</li>
+				</ul>
+			</li>
+			<li><a href="/gallery/Facilities">GALLERY</a></li>
+			<li><a href="/admissions">ADMISSIONS</a></li>
+			<li><a href="/syllabus">SYLLABUS</a></li>
+			<li><a href="/contact_us">CONTACT US</a></li>
+		</ul>
+	</div>
+	<div class="navbar-end">
+		<a class="btn gap-3 rounded-sm btn-accent" href="tel:02241611000">
+			<div class="w-5">
+				<FaPhoneAlt />
+			</div>
+			022 4161 1000
+		</a>
+	</div>
+</nav>
+<main class="mt-36">
 	<slot />
-</div>
+</main>
 <Footer />
