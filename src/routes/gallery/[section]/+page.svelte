7ae254eb90a37c407bbf491f44880dcf359492b6
WhyAsh5114 <whyash5114@gmail.com>
2023-07-11 19:27:37 +0530

    gallery page

---
+++ src/routes/gallery/[section]/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	export let data;
+</script>
+
+<ul class="flex flex-wrap mt-10 gap-0.5">
+	{#each data.items[$page.params['section']] as imgName}
+		<li style="height: 40vh;" class="grow">
+			<img src="/gallery/{$page.params['section']}/{imgName}" alt={imgName} class="max-h-full min-w-full object-cover align-bottom" />
+		</li>
+	{/each}
+	<li />
+</ul>
+
+<style>
+	li:last-child {
+		flex-grow: 10;
+	}
+</style>
