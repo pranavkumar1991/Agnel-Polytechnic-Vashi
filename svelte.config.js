09ea39edc9862a2be2c27a064aa56345f0b4fe6f
WhyAsh5114 <whyash5114@gmail.com>
2023-07-24 20:10:43 +0530

    removed unnecessary code

--- svelte.config.js
+++ svelte.config.js
@@ -8,11 +8,6 @@ const config = {
 		adapter: adapter(),
 		prerender: {
 			handleHttpError: 'warn'
-		},
-		csp: {
-			directives: {
-				'connect-src': ['vitals.vercel-insights.com']
-			}
 		}
 	},
 	preprocess: [
