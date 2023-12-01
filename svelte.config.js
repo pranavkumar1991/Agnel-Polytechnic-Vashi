1ef387325ce9eb6a325b026ef3d9cc234e05fa92
WhyAsh5114 <whyash5114@gmail.com>
2023-12-01 19:04:42 +0530

    removed image preprocessor for now to be added later

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import { importAssets } from 'svelte-preprocess-import-assets';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,7 +9,8 @@ const config = {
 			handleHttpError: 'warn'
 		}
 	},
-	preprocess: [vitePreprocess(), importAssets()]
+	// TODO: add image preprocessing...
+	preprocess: [vitePreprocess()]
 };
 
 export default config;
