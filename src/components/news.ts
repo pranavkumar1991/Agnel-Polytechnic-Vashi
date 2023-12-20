6efb59e6b9c6bc339b18aa2c48efc06a10527937
WhyAsh5114 <whyash5114@gmail.com>
2023-12-20 12:26:11 +0530

    removed duplicates

--- src/components/news.ts
+++ src/components/news.ts
@@ -7,23 +7,5 @@ export const newsArray: News[] = [
 	{
 		title: 'MSBTE Newsletter Oct 2023 News Issue',
 		link: 'https://msbte.org.in/file/MSBTE_Newsletter_Oct_issue_10112023.pdf'
-	},
-	{
-		title: 'Special provision for filling Exam form for Winter 2023 Examination',
-		link: 'https://msbte.org.in/file/MSBTE_Newsletter_Oct_issue_10112023.pdf'
-	},
-	{
-		title: 'Industrial Training Program at M/s. L&T Skills Trainers Academy',
-	},
-	{
-		title: 'MSBTE Newsletter Oct 2023 News Issue',
-		link: 'https://msbte.org.in/file/MSBTE_Newsletter_Oct_issue_10112023.pdf'
-	},
-	{
-		title: 'MSBTE Newsletter Oct 2023 News Issue',
-	},
-	{
-		title: 'MSBTE Newsletter Oct 2023 News Issue',
-		link: 'https://msbte.org.in/file/MSBTE_Newsletter_Oct_issue_10112023.pdf'
 	}
 ];
