213d229e03aa11f6965a3960d86589cbcffdd89c
Your Name here only <krishnsundaram@gmail.com>
2024-03-01 17:09:42 +0530

    removing comment

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,15 +4,14 @@
 	import AllCourses from './AllCourses.svelte';
 	import AboutAgnel from './AboutAgnel.svelte';
 	import Events from './Events.svelte';
-	import PlacementStats from './PlacementStats.svelte';
+	import PlacementStats from './PlacementStats.svelte'c;
 </script>
 
 <div class="-mt-8">
-	<!-- <Hero />
+	<Hero />
 	<AllCourses />
 	<AboutAgnel />
 	<NewsComponent />
 	<Events />
-	<PlacementStats /> -->
-	<h1 class="p-10 font-bold text-5xl w-full text-center">Site Under Maintenance</h1>
+	<PlacementStats />
 </div>
